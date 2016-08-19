7ced1bcd3d811b6be053d6495c1fdd5154825b39
Jorge Escobar <j@jorge3.com>
2016-08-19 14:20:53 -0400

    added flask-cdn and now I'm able to serve content from cloudfron

--- application.py
+++ application.py
@@ -1,20 +1,25 @@
 from flask import Flask
 from flask.ext.mongoengine import MongoEngine
+from flask.ext.cdn import CDN
 
 db = MongoEngine()
+cdn = CDN()
 
 def create_app(**config_overrides):
     app = Flask(__name__)
 
     # Load config
     app.config.from_pyfile('settings.py')
-    
+
     # apply overrides for tests
     app.config.update(config_overrides)
-    
+
     # setup db
     db.init_app(app)
-    
+
+    # setup CDN
+    cdn.init_app(app)
+
     # import blueprints
     from user.views import user_app
     from relationship.views import relationship_app
@@ -26,5 +31,5 @@ def create_app(**config_overrides):
     app.register_blueprint(relationship_app)
     app.register_blueprint(feed_app)
     app.register_blueprint(home_app)
-    
+
     return app
