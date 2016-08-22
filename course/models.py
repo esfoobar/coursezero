174a449156658c64105e315019e116d2e82f2671
Jorge Escobar <j@jorge3.com>
2016-08-22 16:29:43 -0400

    further work on course.models

--- course/models.py
+++ course/models.py
@@ -1,14 +1,19 @@
-
 from application import db
 from user.models import User
 from transaction.models import Transaction
 
 class Track(db.Document):
-    course_list = db.ListField(db_field="cl") # objects with course id and position
-
-class Course(db.Document):
     title = db.StringField(db_field="t")
     subtitle = db.StringField(db_field="st")
+    course_list = db.ListField(db_field="cl") # objects with course id and position
+    image = db.StringField(db_field="i", default=None)
+    live = db.BooleanField(db_field="l", default=True)
+
+class Course(db.Document):
+    track = db.ReferenceField(Track, db_field="tr", reverse_delete_rule=CASCADE)
+    title = db.StringField(db_field="t")
+    subtitle = db.StringField(db_field="st")
+    section_list = db.ListField(db_field="cl") # objects with section id and position
     categories = db.StringField(db_field="c")
     summary = db.StringField(db_field="s")
     price_dollars = db.IntField(db_field="pd")
@@ -24,8 +29,19 @@ class Course(db.Document):
         else:
             return url_for('static', filename=os.path.join(STATIC_IMAGE_URL, 'courses', '%s.%s.%s.png' % (self.id, image_ts, size)))
 
+class Category(db.Document):
+    title = db.StringField(db_field="t")
+
+class Section(db.Document):
+    course = db.ReferenceField(Course, db_field="c", reverse_delete_rule=CASCADE)
+    title = db.StringField(db_field="t")
+    subtitle = db.StringField(db_field="st")
+    lesson_list = db.ListField(db_field="cl") # objects with lesson id and position
+    live = db.BooleanField(db_field="l", default=True)
+
 class Lesson(db.Document):
     course = db.ReferenceField(Course, db_field="c", reverse_delete_rule=CASCADE)
+    section = db.ReferenceField(Section, db_field="s", reverse_delete_rule=CASCADE)
     title = db.StringField(db_field="t")
     summary = db.StringField(db_field="s")
     preview_enabled = db.BooleanField(db_field="pe", default=False)
@@ -36,3 +52,8 @@ class Enrollment(db.Document):
     user = db.ReferenceField(User, db_field="u", reverse_delete_rule=CASCADE)
     coupon_code = db.StringField(db_field="cc")
     transaction = db.ReferenceField(Transaction, db_field="t")
+
+class Progress(db.Document):
+    course = db.ReferenceField(Course, db_field="c", reverse_delete_rule=CASCADE)
+    user = db.ReferenceField(User, db_field="u", reverse_delete_rule=CASCADE)
+    lesson = db.ReferenceField(Lesson, db_field="l", reverse_delete_rule=CASCADE)
