346ea58568a76210a6ecc3a95f523c50879d61f1
Jorge Escobar <j@jorge3.com>
2016-08-26 09:42:44 -0400

    changed home page

--- home/views.py
+++ home/views.py
@@ -9,27 +9,8 @@ home_app = Blueprint('home_app', __name__)
 
 @home_app.route('/')
 def home():
-    if session.get('id'):
-        form = FeedPostForm()
-
-        user = User.objects.filter(
-            id=session.get('id')
-            ).first()
-
-        # get user messages
-        feed_messages = Feed.objects.filter(
-            user=user
-            ).order_by('-create_date')[:10]
-
-        return render_template('home/feed_home.html',
-            user=user,
-            form=form,
-            feed_messages=feed_messages
-        )
-
-    else:
-        home_video_url = get_signed_url('home/the_from_zero_approach.mp4', 5)
-        return render_template('home/home.html', home_video_url=home_video_url)
+    home_video_url = get_signed_url('home/the_from_zero_approach.mp4', 5)
+    return render_template('home/home.html', home_video_url=home_video_url)
 
 @home_app.route('/about')
 def about():
